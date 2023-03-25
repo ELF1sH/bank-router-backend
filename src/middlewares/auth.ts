3c48442c2f27eb2b7e2c87f375095dc42860875c
Igor Lezhnin <r4yson19@gmail.com>
2023-03-26 05:25:13 +0700

    -

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,11 @@
 const auth = (req: any, res: any, next: any) => {
   // res.status(401).send('Error');
+  const accessToken = req.headers['authorization'];
+
+  // TODO: need to be passed into authorization service
+  // TODO: verify that token exists in db and that it hasn't expired
+  console.log(accessToken);
+
   next();
 }
 
