73699637f54644dd94a49ac1a2fcfbd5b41bc15d
Igor Lezhnin <r4yson19@gmail.com>
2023-03-30 02:18:27 +0700

    -

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,15 +4,22 @@ export const auth = async (req: any, res: any, next: any) => {
   const accessToken = req.headers['authorization'];
 
   try {
-    const result = await axios.post(`${process.env.AUTHENTICATION_MS_URL}/isAuthenticated`, {}, {
+    await axios.post(`${process.env.AUTHENTICATION_MS_URL}/isAuthenticated`, {}, {
       headers: {
         Authorization: accessToken,
       },
     });
 
-    if (result.status === 200) {
-      next();
-    }
+    await axios.post(`${process.env.AUTHORIZATION_MS_URL}/isAuthorized`, {
+      requestUrl: req.url,
+      requestBody: req.body,
+    }, {
+      headers: {
+        Authorization: accessToken,
+      },
+    });
+
+    next();
   } catch (e) {
     const error = e as AxiosError;
     res.status(error.response!.status).send(error.response!.data);
